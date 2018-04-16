5b2d438349657534b95cafdd884e64f260e6bc9c
Vadym Zhukovskyy <15155640+vzhukovskyy@users.noreply.github.com>
2018-04-16 12:35:38 -0700

    dynamically switch between simulated and hardware sensors

--- src/sensors/sensors.py
+++ src/sensors/sensors.py
@@ -1,7 +1,10 @@
+import getpass
 import simulated
 import hardware
 
-def get_data():
-    #return simulated.get_data()
-    return hardware.get_data()
 
+def get_data():
+    if getpass.getuser() == 'pi':
+        return hardware.get_data()
+    else:
+        return simulated.get_data()
